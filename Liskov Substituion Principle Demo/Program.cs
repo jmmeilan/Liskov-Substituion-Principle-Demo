b8a63d53614a39b7ca4af06c915b943976f9c8de
Jose Miguel Meilan Maldonado <josemeilan242@gmail.com>
2021-06-09 19:12:43 +0200

    Add project files.

---
+++ Liskov Substituion Principle Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using DemoLibrary;
+using System;
+
+namespace Liskov_Substituion_Principle_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager accountingVP = new Manager();
+
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone";
+            accountingVP.CalculatePerHourRate(4);
+
+            Employee emp = new Manager();
+
+            emp.FirstName = "Tim";
+            emp.LastName = "Corey";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
